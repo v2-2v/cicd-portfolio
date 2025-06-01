66925a13d5fae963a1c28128dfaa814f8ffb0c53
v2-2v <fornotepc7@gmail.com>
2025-06-02 03:24:59 +0900

    set robots.txt

--- file/main.go
+++ file/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+
+	r.GET("/robots.txt", func(c *gin.Context) {
+		c.Header("Content-Type", "text/plain")
+		c.String(http.StatusOK, "User-agent: *\nDisallow: /")
+	})
 
 	r.GET("/", func(c *gin.Context) {
 		c.File("static/index.html")
